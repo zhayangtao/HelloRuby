797f7be20f9b59e270e9ca8058858ab42874fb9b
zhayangtao <1264214725@qq.com>
2017-04-05 23:00:10 +0800

    add test_word.rb

--- helloRuby/robotProgram/test_wordplay.rb
+++ helloRuby/robotProgram/test_wordplay.rb
@@ -1,5 +1,5 @@
 require 'test/unit'
-require 'E:/workspaceForRubyMine/robotProgram/wordplay'
+require 'E:/workspaceForRubyMine/helloRuby/robotProgram/wordplay'
 class TestWordplay < Test::Unit::TestCase
   def test_sentences
     assert_equal(['a', 'b', 'c d', 'e f g'], 'a.b.c d.e f g.'.sentences)
@@ -22,5 +22,10 @@ class TestWordplay < Test::Unit::TestCase
 
   def test_basic_pronouns
     assert_equal('i am a robot', Wordplay.switch_prononus('you are a robot'))
+  end
+
+  def test_mixed_pronouns
+    assert_equal('you gave me life', Wordplay.switch_prononus('i gave you life'))
+    assert_equal('i am not what you are', Wordplay.switch_prononus('you are not what i am'))
   end
 end
